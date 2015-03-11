b9b825d6d53f20f5124c55ef1a923e41bbe00bcb
Abeer Khakwani & Nhu Finney <student@epicodus.com>
2015-03-11 10:52:38 -0700

    Make all 9 tests pass, Test if every 2 entiries possible will work

--- src/Game.php
+++ src/Game.php
@@ -8,19 +8,28 @@ class Game{
 
         $output="";
 
-        if (($input1 == "rock") && ($input2 == "scissors")){
+
+
+      if (($input1 == "rock" && $input2 == "scissors") | ($input1 == "paper" && $input2 == "rock") |($input1 == "scissors" && $input2 == "paper") ){
 
          $output="Player 1";
-        }
+      }
 
+     elseif (($input1 == "scissors" && $input2 == "rock") | ($input1 == "rock" && $input2 == "paper") |($input1 == "paper" && $input2 == "scissors") )
+
+     {
+         $output="Player 2";
+     }
+
+
+     else
+     {
+         $output="Draw";
+    
+
+     }
 
         return $output;
-
-
-
-
-
-
     }
 
 
